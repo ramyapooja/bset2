1131754c46e892377cb299ca607c479eb42508fb
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-08-21 10:38:51 +0530

    Create palindromenum.java

---
+++ palindromenum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+	  int n=sc.nextLine();
+		int temp=n;
+		int r=0,sum=0;
+		while(n!=0)
+		{
+			r=n%10;
+			sum=sum*10+r;
+			n=n/10;
+		}
+		 if(temp==sum)
+		 {
+		 	 System.out.println("yes ");
+		 }
+		 else
+		 {
+		  System.out.println(" no");
+		 
+		 
+		
+		}
+	}
+
+
+}
