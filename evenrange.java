b4396f64d5e55ac8cef0560366bcf83431168e4d
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-08-24 22:36:01 +0530

    Create evenrange.java

---
+++ evenrange.java
@@ -0,0 +1,26 @@
+
+ /* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int m=sc.nextInt();
+		int n=sc.nextInt();
+		for(int i=m+1;i<n;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(i);
+			if(n-i>2)
+				System.out.print(" ");
+			}
+		}
+	}
+}
